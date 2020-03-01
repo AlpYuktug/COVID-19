50320208fcb6126614baef2f3b6ff0a8ba345d4d
AlpYuktug <alperen.altug@gmail.com>
2020-03-02 00:10:01 +0300

    Add News Class

--- app/src/main/java/com/alpyuktug/covid_19/Adapters/RecylerViewAdapterNews.java
+++ app/src/main/java/com/alpyuktug/covid_19/Adapters/RecylerViewAdapterNews.java
@@ -1,4 +1,116 @@
 package com.alpyuktug.covid_19.Adapters;
 
-public class RecylerViewAdapterNews {
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alpyuktug.covid_19.Models.News;
+import com.alpyuktug.covid_19.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RecylerViewAdapterNews extends RecyclerView.Adapter<RecylerViewAdapterNews.NewsViewHolder>{
+
+    Context context;
+    private List<News> NewsList;
+
+    public RecylerViewAdapterNews(List<News> NewsList, Context context) {
+        this.NewsList = NewsList;
+        this.context = context;
+    }
+
+    @Override
+    public RecylerViewAdapterNews.NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_cardview, parent, false);
+        RecylerViewAdapterNews.NewsViewHolder gvh = new RecylerViewAdapterNews.NewsViewHolder(v);
+        return gvh;
+    }
+
+    public void NewsDelete(int position) {
+        NewsList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public void onBindViewHolder(RecylerViewAdapterNews.NewsViewHolder holder, final int position) {
+
+        //holder.imageViewHaberImage.setImageDrawable(NewsList.get(position).getHaberImage());
+        holder.textViewNewsHead.setText(NewsList.get(position).getNewsHead());
+        holder.textViewNewsNumber.setText(NewsList.get(position).getNewsNumber().toString());
+
+        String Image = NewsList.get(position).getNewsImage();
+        Image = Image.replace("https","http");
+
+        Picasso.get()
+                .load(Image)
+                .into(holder.imageViewNewsImage);
+
+        holder.imageViewNewsImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences sharedPref = context.getSharedPreferences("HaberIDDepo",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("HaberNumara",NewsList.get(position).getNewsNumber());
+                editor.commit();
+
+                /*
+                Fragment fragment = new FragmentHaberDetay();
+                FragmentManager fm = ((AnaMenu) context).getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.add(R.id.fragmenticerik, fragment);
+                ft.commit();
+                */
+
+            }
+        });
+
+        holder.textViewNewsHead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences sharedPref = context.getSharedPreferences("HaberIDDepo",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("HaberNumara",NewsList.get(position).getNewsNumber());
+                editor.commit();
+
+                /*
+                Fragment fragment = new FragmentHaberDetay();
+                FragmentManager fm = ((AnaMenu) context).getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.add(R.id.fragmenticerik, fragment);
+                ft.commit();
+
+                 */
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return NewsList.size();
+    }
+
+    public class NewsViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageViewNewsImage;
+        TextView textViewNewsNumber,textViewNewsHead;
+
+        public NewsViewHolder(View view) {
+            super(view);
+            imageViewNewsImage=view.findViewById(R.id.imageViewNewsImage);
+            textViewNewsHead=view.findViewById(R.id.textViewNewsHead);
+            textViewNewsNumber=view.findViewById(R.id.textViewNewsNumber);
+        }
+    }
 }
---
+++ app/src/main/java/com/alpyuktug/covid_19/Fragments/FragmentNewsDetail.java
@@ -0,0 +1,4 @@
+package com.alpyuktug.covid_19.Fragments;
+
+public class FragmentNewsDetail {
+}
