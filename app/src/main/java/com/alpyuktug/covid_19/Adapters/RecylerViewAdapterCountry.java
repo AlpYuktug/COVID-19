693cca0d6f19a66f3db00266d36027a6699c296f
AlpYuktug <alperen.altug@gmail.com>
2020-03-04 22:56:09 +0300

    Country List

---
+++ app/src/main/java/com/alpyuktug/covid_19/Adapters/RecylerViewAdapterCountry.java
@@ -0,0 +1,73 @@
+package com.alpyuktug.covid_19.Adapters;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alpyuktug.covid_19.Activities.MainActivity;
+import com.alpyuktug.covid_19.Models.Covid19Country;
+import com.alpyuktug.covid_19.R;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class RecylerViewAdapterCountry extends RecyclerView.Adapter<RecylerViewAdapterCountry.CountryViewHolder>{
+
+    Context context;
+    private List<Covid19Country> CountryList;
+
+    public RecylerViewAdapterCountry(List<Covid19Country> CountryList, Context context) {
+        this.CountryList = CountryList;
+        this.context = context;
+    }
+
+    @Override
+    public RecylerViewAdapterCountry.CountryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.country_cardview, parent, false);
+
+        RecylerViewAdapterCountry.CountryViewHolder gvh = new RecylerViewAdapterCountry.CountryViewHolder(v);
+        return gvh;
+    }
+
+    public void CountryDelete(int position) {
+        CountryList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public void onBindViewHolder(RecylerViewAdapterCountry.CountryViewHolder holder, final int position) {
+
+        holder.textViewName.setText(CountryList.get(position).getCountryName());
+        holder.textViewTotal.setText(CountryList.get(position).getCountryVirusCount());
+        holder.textViewDead.setText(CountryList.get(position).getCountryVirusDeadCount());
+        holder.textViewRecover.setText(CountryList.get(position).getCountryVirusRecoveredCount());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return CountryList.size();
+    }
+
+    public class CountryViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName,textViewTotal,textViewDead,textViewRecover;
+
+        public CountryViewHolder(View view) {
+            super(view);
+            textViewName=view.findViewById(R.id.textViewName);
+            textViewTotal=view.findViewById(R.id.textViewTotal);
+            textViewDead=view.findViewById(R.id.textViewDead);
+            textViewRecover=view.findViewById(R.id.textViewRecover);
+        }
+    }
+}
---
+++ app/src/main/java/com/alpyuktug/covid_19/Fragments/FragmentCountryList.java
@@ -0,0 +1,104 @@
+package com.alpyuktug.covid_19.Fragments;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import com.alpyuktug.covid_19.Adapters.RecylerViewAdapterCountry;
+import com.alpyuktug.covid_19.Models.CountriesList;
+import com.alpyuktug.covid_19.Models.Covid19Country;
+import com.alpyuktug.covid_19.R;
+import com.alpyuktug.covid_19.Services.ApiUtils;
+import com.alpyuktug.covid_19.Services.AppDAOInerface;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class FragmentCountryList extends Fragment {
+
+    public FragmentCountryList() {
+    }
+
+    private AppDAOInerface countriesDIF;
+    private List<Covid19Country> CountryLists;
+
+    public RecyclerView RecylerViewCountryList;
+    public RecylerViewAdapterCountry CountryAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_countrylist, container, false);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        RecylerViewCountryList = (RecyclerView) view.findViewById(R.id.RecylerViewCountryList);
+        RecylerViewCountryList.setHasFixedSize(true);
+        RecylerViewCountryList.setLayoutManager(new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL));
+
+        countriesDIF = ApiUtils.getCountriesDAOInerface();
+        AllCaountries();
+
+        return view;
+    }
+
+    private void AllCaountries() {
+        if(getString(R.string.language).contains("TR"))
+        {
+            countriesDIF.GetCountriesListTR().enqueue(new Callback<CountriesList>() {
+                @Override
+                public void onResponse(Call<CountriesList> call, Response<CountriesList> response) {
+
+                    CountryLists = response.body().getCovid19Countries();
+
+                    CountryAdapter = new RecylerViewAdapterCountry(CountryLists, getActivity());
+                    RecylerViewCountryList.setAdapter(CountryAdapter);
+                }
+                @Override
+                public void onFailure(Call<CountriesList> call, Throwable t) {
+                }
+            });
+        }
+        else
+        {
+            countriesDIF.GetCountriesListEN().enqueue(new Callback<CountriesList>() {
+                @Override
+                public void onResponse(Call<CountriesList> call, Response<CountriesList> response) {
+
+                    CountryLists = response.body().getCovid19Countries();
+
+                    CountryAdapter = new RecylerViewAdapterCountry(CountryLists, getActivity());
+                    RecylerViewCountryList.setAdapter(CountryAdapter);
+                }
+                @Override
+                public void onFailure(Call<CountriesList> call, Throwable t) {
+                }
+            });
+        }
+    }
+
+}
